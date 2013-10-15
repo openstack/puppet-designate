31302f57770577ff725185627e967ccd93fa6865
Xingchao Yu <xingchao@unitedstack.com>
2013-10-15 11:25:56 +0800

    Add designate sink

---
+++ manifests/sink.pp
@@ -0,0 +1,23 @@
+# Designate sink
+
+class designate::sink (
+  $service_ensure = 'running',
+  $enabled        = true,
+) {
+  include designate::params
+
+  package { 'designate-sink':
+    ensure => installed,
+    name   => $::designate::params::sink_package_name,
+  }
+
+  Package['designate-common'] -> Service['designate-sink']
+
+  service { 'designate-sink':
+    ensure     => $service_ensure,
+    name       => $::designate::params::sink_service_name,
+    enable     => $enabled,
+    hasstatus  => true,
+    hasrestart => true,
+  }
+}
