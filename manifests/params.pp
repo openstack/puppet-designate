d16e9090c78659cf8b432d98a9bc4ccf02cdebf6
Xingchao Yu <xingchao@unitedstack.com>
2013-10-15 11:41:04 +0800

    update params and readme info

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,10 +2,11 @@
 
 class designate::params {
 
-  $dbinit_command =  'designate-manage database-init'
-  $dbsync_command =  'designate-manage database-sync'
-  $state_path     =  '/var/named/data'
-  $log_dir        =  '/var/log/designate'
+  $dbinit_command      =  'designate-manage database-init'
+  $dbsync_command      =  'designate-manage database-sync'
+  $state_path          =  '/var/named/data'
+  $log_dir             =  '/var/log/designate'
+  $client_package_name =  'python-designateclient'
 
   case $::osfamily {
     'RedHat': {
@@ -13,11 +14,13 @@ class designate::params {
        $common_package_name   = 'openstack-designate'
        $api_package_name      = 'openstack-designate-api'
        $central_package_name  = 'openstack-designate-central'
+       $agent_package_name    = 'openstack-designate-agent'
        $sink_package_name     = 'openstack-designate-sink'
        # service names
        $agent_service_name   = 'openstack-designate-agent'
        $api_service_name     = 'openstack-designate-api'
        $central_service_name = 'openstack-designate-central'
+       $sink_service_name    = 'openstack-designate-sink'
     }
   }
 }
