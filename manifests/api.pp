a9f740a70c040887200f4f0c1adc460e237512e4
Xingchao Yu <xingchao@unitedstack.com>
2013-10-18 15:04:04 +0800

    update options in designate::api

--- manifests/api.pp
+++ manifests/api.pp
@@ -1,14 +1,17 @@
+# class designate::api
+
 class designate::api (
   $enabled                    = true,
   $service_ensure             = 'running',
   $auth_strategy              = 'noauth',
   $keystone_host              = '127.0.0.1',
   $keystone_port              = '35357',
-  $keystone_auth_admin_prefix = false,
   $keystone_protocol          = 'http',
-  $keystone_user              = 'ceilometer',
   $keystone_tenant            = 'services',
+  $keystone_user              = 'ceilometer',
   $keystone_password          = false,
+  $enable_api_v1              = true,
+  $enable_api_v2              = false,
 ){
   include designate::params
 
@@ -29,9 +32,21 @@ class designate::api (
     subscribe  => Exec['designate-dbsync']
   }
 
+  # API Service
   designate_config {
-    'service:api/auth_strategy'         : value => $auth_strategy;
+    'service:api/auth_strategy'             : value => $auth_strategy;
+    'service:api/enable_api_v1'             : value => $enable_api_v1;
+    'service:api/enable_api_v2'             : value => $enable_api_v2;
   }
 
+  # Keystone Middleware
+  designate_config {
+    'keystone_authtoken/auth_host'          : value => $keystone_host;
+    'keystone_authtoken/auth_port'          : value => $keystone_port;
+    'keystone_authtoken/auth_protocol'      : value => $keystone_protocol;
+    'keystone_authtoken/admin_tenant_name'  : value => $keystone_tenant;
+    'keystone_authtoken/admin_user'         : value => $keystone_user;
+    'keystone_authtoken/admin_password'     : value => $keystone_password;
+  }
 
 }
