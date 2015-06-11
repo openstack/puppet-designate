7f053f0846c9421cebe50e9f72b1042cb8f42d04
Mathieu Gagné <mgagne@iweb.com>
2015-06-11 14:42:03 -0400

    Introduce public_url, internal_url and admin_url
    This change deprecates the following parameters:
    - version (replaced by public/internal/admin_url)
    - port (replaced by public/internal/admin_url)
    - public_protocol (replaced by public_url)
    - public_address (replaced by public_url)
    - internal_protocol (replaced by internal_url)
    - internal_address (replaced by internal_url)
    - admin_protocol (replaced by admin_url)
    - admin_address (replaced by admin_url)
    
    Add deprecation warnings if any of those values are provided
    while maintaining full backward compatibility.
    
    Change-Id: Iae590b71d5447d19e5e2e64daed49237156e0991
    Closes-bug: #1274979

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -23,35 +23,76 @@
 # [*service_type*]
 #    Type of service. Optional. Defaults to 'metering'.
 #
-# [*public_address*]
-#    Public address for endpoint. Optional. Defaults to '127.0.0.1'.
-#
-# [*admin_address*]
-#    Admin address for endpoint. Optional. Defaults to '127.0.0.1'.
-#
-# [*internal_address*]
-#    Internal address for endpoint. Optional. Defaults to '127.0.0.1'.
-#
-# [*port*]
-#    Port for endpoint. Optional. Defaults to '8777'.
-#
 # [*region*]
 #    Region for endpoint. Optional. Defaults to 'RegionOne'.
 #
 # [*tenant*]
 #    Tenant for designate user. Optional. Defaults to 'services'.
 #
-# [*public_protocol*]
-#    Protocol for public endpoint. Optional. Defaults to 'http'.
+# [*public_url*]
+#   (optional) The endpoint's public url. (Defaults to 'http://127.0.0.1:9001')
+#   This url should *not* contain any trailing '/'.
 #
-# [*admin_protocol*]
-#    Protocol for admin endpoint. Optional. Defaults to 'http'.
+# [*admin_url*]
+#   (optional) The endpoint's admin url. (Defaults to 'http://127.0.0.1:9001')
+#   This url should *not* contain any trailing '/'.
 #
-# [*internal_protocol*]
-#    Protocol for internal endpoint. Optional. Defaults to 'http'.
+# [*internal_url*]
+#   (optional) The endpoint's internal url. (Defaults to 'http://127.0.0.1:9001')
+#   This url should *not* contain any trailing '/'.
 #
 # [*version*]
-#    API version endpoint.  Optional. Defaults to 'v1'.
+#   (optional) DEPRECATED: Use public_url, internal_url and admin_url instead.
+#   API version endpoint. (Defaults to 'v1')
+#   Setting this parameter overrides public_url, internal_url and admin_url parameters.
+#
+# [*port*]
+#   (optional) DEPRECATED: Use public_url, internal_url and admin_url instead.
+#   Default port for endpoints. (Defaults to 9001)
+#   Setting this parameter overrides public_url, internal_url and admin_url parameters.
+#
+# [*public_protocol*]
+#   (optional) DEPRECATED: Use public_url instead.
+#   Protocol for public endpoint. (Defaults to 'http')
+#   Setting this parameter overrides public_url parameter.
+#
+# [*public_address*]
+#   (optional) DEPRECATED: Use public_url instead.
+#   Public address for endpoint. (Defaults to '127.0.0.1')
+#   Setting this parameter overrides public_url parameter.
+#
+# [*internal_protocol*]
+#   (optional) DEPRECATED: Use internal_url instead.
+#   Protocol for internal endpoint. (Defaults to 'http')
+#   Setting this parameter overrides internal_url parameter.
+#
+# [*internal_address*]
+#   (optional) DEPRECATED: Use internal_url instead.
+#   Internal address for endpoint. (Defaults to '127.0.0.1')
+#   Setting this parameter overrides internal_url parameter.
+#
+# [*admin_protocol*]
+#   (optional) DEPRECATED: Use admin_url instead.
+#   Protocol for admin endpoint. (Defaults to 'http')
+#   Setting this parameter overrides admin_url parameter.
+#
+# [*admin_address*]
+#   (optional) DEPRECATED: Use admin_url instead.
+#   Admin address for endpoint. (Defaults to '127.0.0.1')
+#   Setting this parameter overrides admin_url parameter.
+#
+# === Deprecation notes
+#
+# If any value is provided for public_protocol, public_address or port parameters,
+# public_url will be completely ignored. The same applies for internal and admin parameters.
+#
+# === Examples
+#
+#  class { 'designate::keystone::auth':
+#    public_url   => 'https://10.0.0.10:9001',
+#    internal_url => 'https://10.0.0.11:9001',
+#    admin_url    => 'https://10.0.0.11:9001',
+#  }
 #
 class designate::keystone::auth (
   $password           = false,
@@ -59,18 +100,84 @@ class designate::keystone::auth (
   $auth_name          = 'designate',
   $service_name       = undef,
   $service_type       = 'dns',
-  $public_address     = '127.0.0.1',
-  $admin_address      = '127.0.0.1',
-  $internal_address   = '127.0.0.1',
-  $port               = '9001',
   $region             = 'RegionOne',
   $tenant             = 'services',
-  $public_protocol    = 'http',
-  $admin_protocol     = 'http',
-  $internal_protocol  = 'http',
-  $version            = 'v1',
-  $configure_endpoint = true
+  $configure_endpoint = true,
+  $public_url         = 'http://127.0.0.1:9001/v1',
+  $admin_url          = 'http://127.0.0.1:9001/v1',
+  $internal_url       = 'http://127.0.0.1:9001/v1',
+  # DEPRECATED PARAMETERS
+  $version            = undef,
+  $port               = undef,
+  $public_protocol    = undef,
+  $public_address     = undef,
+  $internal_protocol  = undef,
+  $internal_address   = undef,
+  $admin_protocol     = undef,
+  $admin_address      = undef,
 ) {
+
+  if $version {
+    warning('The version parameter is deprecated, use public_url, internal_url and admin_url instead.')
+  }
+
+  if $port {
+    warning('The port parameter is deprecated, use public_url, internal_url and admin_url instead.')
+  }
+
+  if $public_protocol {
+    warning('The public_protocol parameter is deprecated, use public_url instead.')
+  }
+
+  if $internal_protocol {
+    warning('The internal_protocol parameter is deprecated, use internal_url instead.')
+  }
+
+  if $admin_protocol {
+    warning('The admin_protocol parameter is deprecated, use admin_url instead.')
+  }
+
+  if $public_address {
+    warning('The public_address parameter is deprecated, use public_url instead.')
+  }
+
+  if $internal_address {
+    warning('The internal_address parameter is deprecated, use internal_url instead.')
+  }
+
+  if $admin_address {
+    warning('The admin_address parameter is deprecated, use admin_url instead.')
+  }
+
+  if ($public_protocol or $public_address or $port or $version) {
+    $public_url_real = sprintf('%s://%s:%s/%s',
+      pick($public_protocol, 'http'),
+      pick($public_address, '127.0.0.1'),
+      pick($port, '9001'),
+      pick($version, 'v1'))
+  } else {
+    $public_url_real = $public_url
+  }
+
+  if ($admin_protocol or $admin_address or $port or $version) {
+    $admin_url_real = sprintf('%s://%s:%s/%s',
+      pick($admin_protocol, 'http'),
+      pick($admin_address, '127.0.0.1'),
+      pick($port, '9001'),
+      pick($version, 'v1'))
+  } else {
+    $admin_url_real = $admin_url
+  }
+
+  if ($internal_protocol or $internal_address or $port or $version) {
+    $internal_url_real = sprintf('%s://%s:%s/%s',
+      pick($internal_protocol, 'http'),
+      pick($internal_address, '127.0.0.1'),
+      pick($port, '9001'),
+      pick($version, 'v1'))
+  } else {
+    $internal_url_real = $internal_url
+  }
 
   $real_service_name = pick($service_name, $auth_name)
 
@@ -89,9 +196,9 @@ class designate::keystone::auth (
     password            => $password,
     email               => $email,
     tenant              => $tenant,
-    public_url          => "${public_protocol}://${public_address}:${port}/${version}",
-    internal_url        => "${internal_protocol}://${internal_address}:${port}/${version}",
-    admin_url           => "${admin_protocol}://${admin_address}:${port}/${version}",
+    public_url          => $public_url_real,
+    internal_url        => $internal_url_real,
+    admin_url           => $admin_url_real,
   }
 
 }
