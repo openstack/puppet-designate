aa4184a043550536318a05a613e515168879e144
Yanis Guenane <yguenane@redhat.com>
2015-08-06 13:05:36 +0200

    Reflect provider change in puppet-openstacklib
    With the creation of the new openstack_config provider, some processing
    that was done in designate_config has been centralized in
    openstack_config.
    
    Impacted methods are :
    
      * section
      * setting
      * separator
    
    Also, this commit adds the fact that, when passing a specific string
    (ensure_absent_val) the provider will behave as if ensure => absent was
    specified. '<SERVICE DEFAULT>' is the default value for
    ensure_absent_val.
    
    The use case is the following :
    
    designate_config { 'DEFAULT/foo' : value => 'bar' } # will work as usual
    
    designate_config { 'DEFAULT/foo' : value => '<SERVICE DEFAULT>' } # will mean absent
    
    That means that all the current :
    
    if $myvar {
      designate_config { 'DEFAULT/foo' : value => $myvar }
    } else {
      designate_config { 'DEFAULT/foo' : ensure => absent }
    }
    
    can be removed in favor of :
    
    designate_config { 'DEFAULT/foo' : value => $myvar }
    
    If for any reason '<SERVICE DEFAULT>' turns out to be a valid value for
    a specific parameter. One could by pass that doing the following :
    
    designate_config { 'DEFAULT/foo' : value => '<SERVICE DEFAULT>',
    ensure_absent_val => 'foo' }
    
    Change-Id: I16873c60bfce5b2f432f034755b7bfa3f1383dc5
    Depends-On: I0eeebde3aac2662cc7e69bfad7f8d2481463a218

---
+++ spec/acceptance/designate_config_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper_acceptance'
+
+describe 'basic designate_config resource' do
+
+  context 'default parameters' do
+
+    it 'should work with no errors' do
+      pp= <<-EOS
+      Exec { logoutput => 'on_failure' }
+
+      File <||> -> Designate_config <||>
+
+      file { '/etc/designate' :
+        ensure => directory,
+      }
+      file { '/etc/designate/designate.conf' :
+        ensure => file,
+      }
+
+      designate_config { 'DEFAULT/thisshouldexist' :
+        value => 'foo',
+      }
+
+      designate_config { 'DEFAULT/thisshouldnotexist' :
+        value => '<SERVICE DEFAULT>',
+      }
+
+      designate_config { 'DEFAULT/thisshouldexist2' :
+        value             => '<SERVICE DEFAULT>',
+        ensure_absent_val => 'toto',
+      }
+
+      designate_config { 'DEFAULT/thisshouldnotexist2' :
+        value             => 'toto',
+        ensure_absent_val => 'toto',
+      }
+      EOS
+
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe file('/etc/designate/designate.conf') do
+      it { should exist }
+      it { should contain('thisshouldexist=foo') }
+      it { should contain('thisshouldexist2=<SERVICE DEFAULT>') }
+
+      its(:content) { should_not match /thisshouldnotexist/ }
+    end
+
+
+  end
+end
