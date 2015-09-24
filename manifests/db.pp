135a1aca3bcd90a4dc4857cdfe8402bbd88abf15
Yanis Guenane <yguenane@redhat.com>
2015-09-24 12:46:55 +0200

    Update designate::db class to match other module pattern
    This review aims to update the designate::db class to standardize it with the
    other modules db classes[1][2]. It now offers the same parameters to customize
    database connection behavior.  Some internal variable naming remains
    different then other modules.  This is because designate simply uses
    differen config file sections[3] then other OpenStack projects.
    
    [1] https://github.com/openstack/puppet-neutron/blob/master/manifests/db.pp
    [2] https://github.com/openstack/puppet-sahara/blob/master/manifests/db.pp
    [3] http://docs.openstack.org/developer/designate/install/ubuntu-kilo.html#installing-designate
    
    Co-Authored-By: Cody Herriges <cody@puppetlabs.com>
    Change-Id: Ieb841a58429564e5f13d9f683a3a9ef875f1324a

--- manifests/db.pp
+++ manifests/db.pp
@@ -7,21 +7,61 @@
 #
 # == Parameters
 #
-#  [*database_connection*]
-#    the connection string. format: [driver]://[user]:[password]@[host]/[database]
+# [*database_connection*]
+#   Url used to connect to database.
+#   (Optional) Defaults to 'mysql://designate:designate@localhost/designate'.
 #
-#  [*sync_db*]
-#    Enable dbsync.
+# [*database_idle_timeout*]
+#   Timeout when db connections should be reaped.
+#   (Optional) Defaults to 3600.
+#
+# [*database_min_pool_size*]
+#   Minimum number of SQL connections to keep open in a pool.
+#   (Optional) Defaults to 1.
+#
+# [*database_max_pool_size*]
+#   Maximum number of SQL connections to keep open in a pool.
+#   (Optional) Defaults to 10.
+#
+# [*database_max_retries*]
+#   Maximum db connection retries during startup.
+#   Setting -1 implies an infinite retry count.
+#   (Optional) Defaults to 10.
+#
+# [*database_retry_interval*]
+#   Interval between retries of opening a sql connection.
+#   (Optional) Defaults to 10.
+#
+# [*database_max_overflow*]
+#   If set, use this value for max_overflow with sqlalchemy.
+#   (Optional) Defaults to 20.
+#
+# [*sync_db*]
+#   (Optional) Run db sync on nodes after connection setting has been set.
+#   Defaults to true
+#
+# [*sync_db*]
+#   Enable dbsync.
 #
 class designate::db (
-  $database_connection = 'mysql://designate:designate@localhost/designate',
-  $sync_db             = true,
+  $database_connection     = 'mysql://designate:designate@localhost/designate',
+  $database_idle_timeout   = 3600,
+  $database_min_pool_size  = 1,
+  $database_max_pool_size  = 10,
+  $database_max_retries    = 10,
+  $database_retry_interval = 10,
+  $database_max_overflow   = 20,
+  $sync_db                 = true,
 ) {
 
   include ::designate::params
 
+  validate_re($database_connection,
+    '(mysql):\/\/(\S+:\S+@\S+\/\S+)?')
+
   case $database_connection {
     /^mysql:\/\//: {
+      $backend_package = false
       require 'mysql::bindings'
       require 'mysql::bindings::python'
     }
@@ -30,8 +70,22 @@ class designate::db (
     }
   }
 
+  if $backend_package and !defined(Package[$backend_package]) {
+    package {'designate-backend-package':
+      ensure => present,
+      name   => $backend_package,
+      tag    => 'openstack',
+    }
+  }
+
   designate_config {
-    'storage:sqlalchemy/connection': value => $database_connection, secret => true;
+    'storage:sqlalchemy/connection':     value => $database_connection, secret => true;
+    'storage:sqlalchemy/idle_timeout':   value => $database_idle_timeout;
+    'storage:sqlalchemy/min_pool_size':  value => $database_min_pool_size;
+    'storage:sqlalchemy/max_retries':    value => $database_max_retries;
+    'storage:sqlalchemy/retry_interval': value => $database_retry_interval;
+    'storage:sqlalchemy/max_pool_size':  value => $database_max_pool_size;
+    'storage:sqlalchemy/max_overflow':   value => $database_max_overflow;
   }
 
   if $sync_db {
