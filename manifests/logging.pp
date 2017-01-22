6d0d2f2cd67b4507ab25bc6793cbd11d904cc439
ZhongShengping <chdzsp@163.com>
2017-01-22 16:23:14 +0800

    Switch to puppet-oslo configuring logging
    1) Switch to puppet-oslo configuring logging.
    2) switch messaging related parameters default value to
       $::os_service_default.
    3) Update releated unit test.
    
    Change-Id: I60e38b8fe26fede0829b24ccc9cffd1a05bc4ed7

--- manifests/logging.pp
+++ manifests/logging.pp
@@ -6,19 +6,19 @@
 #
 #  [*debug*]
 #    (Optional) Should the daemons log debug messages
-#    Defaults to 'false'
+#    Defaults to $::os_service_default
 #
 #  [*use_syslog*]
 #    (Optional) Use syslog for logging.
-#    Defaults to 'false'
+#    Defaults to $::os_service_default
 #
 #  [*use_stderr*]
 #    (optional) Use stderr for logging
-#    Defaults to 'true'
+#    Defaults to $::os_service_default
 #
 #  [*log_facility*]
 #    (Optional) Syslog facility to receive log lines.
-#    Defaults to 'LOG_USER'
+#    Defaults to $::os_service_default
 #
 #  [*log_dir*]
 #    (optional) Directory where logs should be stored.
@@ -27,34 +27,34 @@
 #
 #  [*logging_context_format_string*]
 #    (optional) Format string to use for log messages with context.
-#    Defaults to undef.
+#    Defaults to $::os_service_default
 #    Example: '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s\
 #              [%(request_id)s %(user_identity)s] %(instance)s%(message)s'
 #
 #  [*logging_default_format_string*]
 #    (optional) Format string to use for log messages without context.
-#    Defaults to undef.
+#    Defaults to $::os_service_default
 #    Example: '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s\
 #              [-] %(instance)s%(message)s'
 #
 #  [*logging_debug_format_suffix*]
 #    (optional) Formatted data to append to log format when level is DEBUG.
-#    Defaults to undef.
+#    Defaults to $::os_service_default
 #    Example: '%(funcName)s %(pathname)s:%(lineno)d'
 #
 #  [*logging_exception_prefix*]
 #    (optional) Prefix each line of exception output with this format.
-#    Defaults to undef.
+#    Defaults to $::os_service_default
 #    Example: '%(asctime)s.%(msecs)03d %(process)d TRACE %(name)s %(instance)s'
 #
 #  [*log_config_append*]
 #    The name of an additional logging configuration file.
-#    Defaults to undef.
+#    Defaults to $::os_service_default
 #    See https://docs.python.org/2/howto/logging.html
 #
 #  [*default_log_levels*]
 #    (optional) Hash of logger (keys) and level (values) pairs.
-#    Defaults to undef.
+#    Defaults to $::os_service_default
 #    Example:
 #      { 'amqp' => 'WARN', 'amqplib' => 'WARN', 'boto' => 'WARN',
 #        'sqlalchemy' => 'WARN', 'suds' => 'INFO', 'iso8601' => 'WARN',
@@ -62,47 +62,47 @@
 #
 #  [*publish_errors*]
 #    (optional) Publish error events (boolean value).
-#    Defaults to undef (false if unconfigured).
+#    Defaults to $::os_service_default
 #
 #  [*fatal_deprecations*]
 #    (optional) Make deprecations fatal (boolean value)
-#    Defaults to undef (false if unconfigured).
+#    Defaults to $::os_service_default
 #
 #  [*instance_format*]
 #    (optional) If an instance is passed with the log message, format it
 #               like this (string value).
-#    Defaults to undef.
+#    Defaults to $::os_service_default
 #    Example: '[instance: %(uuid)s] '
 #
 #  [*instance_uuid_format*]
 #    (optional) If an instance UUID is passed with the log message, format
 #               it like this (string value).
-#    Defaults to undef.
+#    Defaults to $::os_service_default
 #    Example: instance_uuid_format='[instance: %(uuid)s] '
 #
 #  [*log_date_format*]
 #    (optional) Format string for %%(asctime)s in log records.
-#    Defaults to undef.
+#    Defaults to $::os_service_default
 #    Example: 'Y-%m-%d %H:%M:%S'
 #
 class designate::logging(
-  $use_syslog                    = false,
-  $use_stderr                    = true,
-  $log_facility                  = 'LOG_USER',
+  $use_syslog                    = $::os_service_default,
+  $use_stderr                    = $::os_service_default,
+  $log_facility                  = $::os_service_default,
   $log_dir                       = $::designate::params::log_dir,
-  $debug                         = false,
-  $logging_context_format_string = undef,
-  $logging_default_format_string = undef,
-  $logging_debug_format_suffix   = undef,
-  $logging_exception_prefix      = undef,
-  $log_config_append             = undef,
-  $default_log_levels            = undef,
-  $publish_errors                = undef,
-  $fatal_deprecations            = undef,
-  $instance_format               = undef,
-  $instance_uuid_format          = undef,
-  $log_date_format               = undef,
-) inherits ::designate::params  {
+  $debug                         = $::os_service_default,
+  $logging_context_format_string = $::os_service_default,
+  $logging_default_format_string = $::os_service_default,
+  $logging_debug_format_suffix   = $::os_service_default,
+  $logging_exception_prefix      = $::os_service_default,
+  $log_config_append             = $::os_service_default,
+  $default_log_levels            = $::os_service_default,
+  $publish_errors                = $::os_service_default,
+  $fatal_deprecations            = $::os_service_default,
+  $instance_format               = $::os_service_default,
+  $instance_uuid_format          = $::os_service_default,
+  $log_date_format               = $::os_service_default,
+) inherits ::designate::params {
 
   include ::designate::deps
 
@@ -114,138 +114,23 @@ class designate::logging(
   $log_dir_real = pick($::designate::log_dir, $log_dir)
   $debug_real = pick($::designate::debug,$debug)
 
-  designate_config {
-    'DEFAULT/debug'              : value => $debug_real;
-    'DEFAULT/use_stderr'         : value => $use_stderr_real;
-    'DEFAULT/use_syslog'         : value => $use_syslog_real;
-    'DEFAULT/log_dir'            : value => $log_dir_real;
-    'DEFAULT/syslog_log_facility': value => $log_facility_real;
+  oslo::log { 'designate_config':
+    debug                         => $debug_real,
+    use_syslog                    => $use_syslog_real,
+    use_stderr                    => $use_stderr_real,
+    log_dir                       => $log_dir_real,
+    syslog_log_facility           => $log_facility_real,
+    logging_context_format_string => $logging_context_format_string,
+    logging_default_format_string => $logging_default_format_string,
+    logging_debug_format_suffix   => $logging_debug_format_suffix,
+    logging_exception_prefix      => $logging_exception_prefix,
+    log_config_append             => $log_config_append,
+    default_log_levels            => $default_log_levels,
+    publish_errors                => $publish_errors,
+    fatal_deprecations            => $fatal_deprecations,
+    log_date_format               => $log_date_format,
+    instance_format               => $instance_format,
+    instance_uuid_format          => $instance_uuid_format,
   }
-
-  if $logging_context_format_string {
-    designate_config {
-      'DEFAULT/logging_context_format_string' :
-        value => $logging_context_format_string;
-      }
-    }
-  else {
-    designate_config {
-      'DEFAULT/logging_context_format_string' : ensure => absent;
-      }
-    }
-
-  if $logging_default_format_string {
-    designate_config {
-      'DEFAULT/logging_default_format_string' :
-        value => $logging_default_format_string;
-      }
-    }
-  else {
-    designate_config {
-      'DEFAULT/logging_default_format_string' : ensure => absent;
-      }
-    }
-
-  if $logging_debug_format_suffix {
-    designate_config {
-      'DEFAULT/logging_debug_format_suffix' :
-        value => $logging_debug_format_suffix;
-      }
-    }
-  else {
-    designate_config {
-      'DEFAULT/logging_debug_format_suffix' : ensure => absent;
-      }
-    }
-
-  if $logging_exception_prefix {
-    designate_config {
-      'DEFAULT/logging_exception_prefix' : value => $logging_exception_prefix;
-      }
-    }
-  else {
-    designate_config {
-      'DEFAULT/logging_exception_prefix' : ensure => absent;
-      }
-    }
-
-  if $log_config_append {
-    designate_config {
-      'DEFAULT/log_config_append' : value => $log_config_append;
-      }
-    }
-  else {
-    designate_config {
-      'DEFAULT/log_config_append' : ensure => absent;
-      }
-    }
-
-  if $default_log_levels {
-    designate_config {
-      'DEFAULT/default_log_levels' :
-        value => join(sort(join_keys_to_values($default_log_levels, '=')), ',');
-      }
-    }
-  else {
-    designate_config {
-      'DEFAULT/default_log_levels' : ensure => absent;
-      }
-    }
-
-  if $publish_errors {
-    designate_config {
-      'DEFAULT/publish_errors' : value => $publish_errors;
-      }
-    }
-  else {
-    designate_config {
-      'DEFAULT/publish_errors' : ensure => absent;
-      }
-    }
-
-  if $fatal_deprecations {
-    designate_config {
-      'DEFAULT/fatal_deprecations' : value => $fatal_deprecations;
-      }
-    }
-  else {
-    designate_config {
-      'DEFAULT/fatal_deprecations' : ensure => absent;
-      }
-    }
-
-  if $instance_format {
-    designate_config {
-      'DEFAULT/instance_format' : value => $instance_format;
-      }
-    }
-  else {
-    designate_config {
-      'DEFAULT/instance_format' : ensure => absent;
-      }
-    }
-
-  if $instance_uuid_format {
-    designate_config {
-      'DEFAULT/instance_uuid_format' : value => $instance_uuid_format;
-      }
-    }
-  else {
-    designate_config {
-      'DEFAULT/instance_uuid_format' : ensure => absent;
-      }
-    }
-
-  if $log_date_format {
-    designate_config {
-      'DEFAULT/log_date_format' : value => $log_date_format;
-      }
-    }
-  else {
-    designate_config {
-      'DEFAULT/log_date_format' : ensure => absent;
-      }
-    }
-
 
 }
