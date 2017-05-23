877f6ca1d1343ab9d691d1a8f7509d9f9ba21344
ZhongShengping <chdzsp@163.com>
2017-05-23 14:05:42 +0800

    oslo policy: check puppet resource instead of actual config in spec
    Change-Id: I494ed6b2d9c2e0a84380cb1cce7dbec6f0fc0034

--- spec/classes/designate_policy_spec.rb
+++ spec/classes/designate_policy_spec.rb
@@ -20,7 +20,9 @@ describe 'designate::policy' do
         :key   => 'context_is_admin',
         :value => 'foo:bar'
       })
-      is_expected.to contain_designate_config('oslo_policy/policy_file').with_value('/etc/designate/policy.json')
+      is_expected.to contain_oslo__policy('designate_config').with(
+        :policy_file => '/etc/designate/policy.json',
+      )
     end
   end
 
