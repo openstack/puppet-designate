270465c7fef921fe4e8f4bb498337f74966f9fad
Takashi Kajinami <tkajinam@redhat.com>
2020-04-25 23:33:40 +0900

    Cleanup the remaining parameters for designate pool manager
    Support for designate pool manager was already removed[1], so we can
    remove some parameters for it.
    
    [1] 91c634593e0c24d83067f4297e9fca477d330ac3
    
    Change-Id: I9776b4b015a280f6e5f8441caccf896d4de38c44

--- manifests/params.pp
+++ manifests/params.pp
@@ -14,7 +14,6 @@ class designate::params {
   $central_service_name      = 'designate-central'
   $sink_service_name         = 'designate-sink'
   $mdns_service_name         = 'designate-mdns'
-  $pool_manager_service_name = 'designate-pool-manager'
   $zone_manager_service_name = 'designate-zone-manager'
   $producer_service_name     = 'designate-producer'
   $worker_service_name       = 'designate-worker'
@@ -30,7 +29,6 @@ class designate::params {
       $sink_package_name         = 'openstack-designate-sink'
       $pymysql_package_name      = undef
       $mdns_package_name         = 'openstack-designate-mdns'
-      $pool_manager_package_name = 'openstack-designate-pool-manager'
       $zone_manager_package_name = 'openstack-designate-zone-manager'
       $producer_package_name     = 'openstack-designate-producer'
       $worker_package_name       = 'openstack-designate-worker'
@@ -43,7 +41,6 @@ class designate::params {
       $agent_package_name        = 'designate-agent'
       $sink_package_name         = 'designate-sink'
       $pymysql_package_name      = "python${pyvers}-pymysql"
-      $pool_manager_package_name = 'designate-pool-manager'
       $mdns_package_name         = 'designate-mdns'
       $zone_manager_package_name = 'designate-zone-manager'
       $producer_package_name     = 'designate-producer'
