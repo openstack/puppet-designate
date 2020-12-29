702128cad5407d264170641c5b0411135b769c5b
Takashi Kajinami <tkajinam@redhat.com>
2020-12-29 15:58:56 +0900

    Use yaml instead of json for policy file
    Because usage of json for policy file will be deprecated and replaced
    by yaml[1].
    
    [1] https://governance.openstack.org/tc/goals/selected/wallaby/migrate-policy-format-from-json-to-yaml.html
    
    Depends-on: https://review.opendev.org/769647
    Change-Id: I1ebd0852e7a39063115f18bed2c9333abf748795

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -20,12 +20,12 @@
 #   Defaults to empty hash.
 #
 # [*policy_path*]
-#   (Optional) Path to the designate policy.json file
-#   Defaults to /etc/designate/policy.json
+#   (Optional) Path to the designate policy.yaml file
+#   Defaults to /etc/designate/policy.yaml
 #
 class designate::policy (
   $policies    = {},
-  $policy_path = '/etc/designate/policy.json',
+  $policy_path = '/etc/designate/policy.yaml',
 ) {
 
   include designate::deps
@@ -34,9 +34,10 @@ class designate::policy (
   validate_legacy(Hash, 'validate_hash', $policies)
 
   Openstacklib::Policy::Base {
-    file_path  => $policy_path,
-    file_user  => 'root',
-    file_group => $::designate::params::group,
+    file_path   => $policy_path,
+    file_user   => 'root',
+    file_group  => $::designate::params::group,
+    file_format => 'yaml',
   }
 
   create_resources('openstacklib::policy::base', $policies)
