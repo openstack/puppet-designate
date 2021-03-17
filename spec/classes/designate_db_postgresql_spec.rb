47e3e4132fbdf046c0d1f8dcfe1dd2b08b73778f
Christian Rohmann <christian.rohmann@inovex.de>
2021-03-17 11:34:27 +0100

    Add designate::db::postgresql class.
    This allows Designate to be used with PostgreSQL.
    
    Change-Id: Ief63f88ba323e3f20300b8ff6a5148a78413ac3d
    (cherry picked from commit fd43d18c0f83c3401c7908fa60f160e4ae7ab4a2)

---
+++ spec/classes/designate_db_postgresql_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe 'designate::db::postgresql' do
+
+  shared_examples 'designate::db::postgresql' do
+    let :req_params do
+      { :password => 'pw' }
+    end
+
+    let :pre_condition do
+      'include postgresql::server'
+    end
+
+    context 'with only required parameters' do
+      let :params do
+        req_params
+      end
+
+      it { is_expected.to contain_class('designate::deps') }
+
+      it { should contain_openstacklib__db__postgresql('designate').with(
+        :password   => 'pw',
+        :dbname     => 'designate',
+        :user       => 'designate',
+        :encoding   => nil,
+        :privileges => 'ALL',
+      )}
+    end
+  end
+
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge(OSDefaults.get_facts({
+          :os_workers     => 8,
+          :concat_basedir => '/var/lib/puppet/concat'
+        }))
+      end
+
+      it_configures 'designate::db::postgresql'
+    end
+  end
+end
