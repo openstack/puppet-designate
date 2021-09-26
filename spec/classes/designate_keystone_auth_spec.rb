3e48d3f6dbe1898ab8f78f1e0b79c8556308ad25
Takashi Kajinami <tkajinam@redhat.com>
2021-09-27 08:46:39 +0900

    Do not test detail of keystone::resource::service_identity
    ... but test interfaces of that resource type. This helps us avoid
    direct breakage caused by any change in puppet-keystone.
    
    Change-Id: I13f47eb9068a023384817b36e94fc687f1b378cc

--- spec/classes/designate_keystone_auth_spec.rb
+++ spec/classes/designate_keystone_auth_spec.rb
@@ -1,83 +1,69 @@
 #
 # Unit tests for designate::keystone::auth
 #
+
 require 'spec_helper'
 
 describe 'designate::keystone::auth' do
-  shared_examples_for 'designate-keystone-auth' do
+  shared_examples_for 'designate::keystone::auth' do
     context 'with default class parameters' do
       let :params do
-        { :password => 'desigpwd',
-          :tenant   => 'fooboozoo' }
+        { :password => 'designate_password' }
       end
 
-      it { is_expected.to contain_keystone_user('designate').with(
-        :ensure   => 'present',
-        :password => 'desigpwd',
-      ) }
-
-      it { is_expected.to contain_keystone_user_role('designate@fooboozoo').with(
-        :ensure  => 'present',
-        :roles   => ['admin']
-      )}
-
-      it { is_expected.to contain_keystone_service('designate::dns').with(
-        :ensure      => 'present',
-        :description => 'Openstack DNSaas Service'
-      ) }
-
-      it { is_expected.to contain_keystone_endpoint('RegionOne/designate::dns').with(
-        :ensure       => 'present',
-        :public_url   => "http://127.0.0.1:9001",
-        :admin_url    => "http://127.0.0.1:9001",
-        :internal_url => "http://127.0.0.1:9001"
+      it { is_expected.to contain_keystone__resource__service_identity('designate').with(
+        :configure_user      => true,
+        :configure_user_role => true,
+        :configure_endpoint  => true,
+        :service_name        => 'designate',
+        :service_type        => 'dns',
+        :service_description => 'Openstack DNSaas Service',
+        :region              => 'RegionOne',
+        :auth_name           => 'designate',
+        :password            => 'designate_password',
+        :email               => 'designate@localhost',
+        :tenant              => 'services',
+        :public_url          => 'http://127.0.0.1:9001',
+        :internal_url        => 'http://127.0.0.1:9001',
+        :admin_url           => 'http://127.0.0.1:9001',
       ) }
     end
 
-
-    context 'when overriding endpoint URLs' do
+    context 'when overriding parameters' do
       let :params do
-        { :password         => 'desigpwd',
-          :public_url       => 'https://10.10.10.10:81/v2',
-          :internal_url     => 'https://10.10.10.11:81/v2',
-          :admin_url        => 'https://10.10.10.12:81/v2' }
+        { :password            => 'designate_password',
+          :auth_name           => 'alt_designate',
+          :email               => 'alt_designate@alt_localhost',
+          :tenant              => 'alt_service',
+          :configure_endpoint  => false,
+          :configure_user      => false,
+          :configure_user_role => false,
+          :service_description => 'Alternative Openstack DNSaas Service',
+          :service_name        => 'alt_service',
+          :service_type        => 'alt_dns',
+          :region              => 'RegionTwo',
+          :public_url          => 'https://10.10.10.10:80',
+          :internal_url        => 'http://10.10.10.11:81',
+          :admin_url           => 'http://10.10.10.12:81' }
       end
 
-      it { is_expected.to contain_keystone_endpoint('RegionOne/designate::dns').with(
-        :ensure       => 'present',
-        :public_url   => 'https://10.10.10.10:81/v2',
-        :internal_url => 'https://10.10.10.11:81/v2',
-        :admin_url    => 'https://10.10.10.12:81/v2'
-      ) }
-    end
-
-    context 'when overriding auth and service name' do
-      let :params do
-        { :password => 'foo',
-          :service_name => 'designatey',
-          :auth_name => 'designatey' }
-      end
-
-      it { is_expected.to contain_keystone_user('designatey') }
-      it { is_expected.to contain_keystone_user_role('designatey@services') }
-      it { is_expected.to contain_keystone_service('designatey::dns') }
-      it { is_expected.to contain_keystone_endpoint('RegionOne/designatey::dns') }
-    end
-
-  end
-
-  describe 'when disabling user and user_role configuration' do
-    let :params do
-      { :configure_user      => false,
+      it { is_expected.to contain_keystone__resource__service_identity('designate').with(
+        :configure_user      => false,
         :configure_user_role => false,
-        :service_name => 'designate',
-        :auth_name => 'designate',
-        :password            => 'designate_password' }
+        :configure_endpoint  => false,
+        :service_name        => 'alt_service',
+        :service_type        => 'alt_dns',
+        :service_description => 'Alternative Openstack DNSaas Service',
+        :region              => 'RegionTwo',
+        :auth_name           => 'alt_designate',
+        :password            => 'designate_password',
+        :email               => 'alt_designate@alt_localhost',
+        :tenant              => 'alt_service',
+        :public_url          => 'https://10.10.10.10:80',
+        :internal_url        => 'http://10.10.10.11:81',
+        :admin_url           => 'http://10.10.10.12:81',
+      ) }
     end
-    it { is_expected.to_not contain_keystone_user('designate') }
-    it { is_expected.to_not contain_keystone_user_role('designate@services') }
-    it { is_expected.to contain_keystone_service('designate::dns') }
-    it { is_expected.to contain_keystone_endpoint('RegionOne/designate::dns') }
   end
 
   on_supported_os({
@@ -88,7 +74,7 @@ describe 'designate::keystone::auth' do
         facts.merge!(OSDefaults.get_facts())
       end
 
-      it_behaves_like 'designate-keystone-auth'
+      it_behaves_like 'designate::keystone::auth'
     end
   end
 end
