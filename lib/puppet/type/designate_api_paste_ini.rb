2e4b2c046bf3b9c5a4306e03f547ec14d025c579
Takashi Kajinami <tkajinam@redhat.com>
2021-11-01 22:04:48 +0900

    Allow customizing separator for api-paste.ini
    The api-paste.ini accepts not only "=" but also ":" and some services
    like Barbican have been using ":" for their default api-paste.ini
    files.
    
    [composite:main]
    use = egg:Paste#urlmap
    /: barbican_version
    /v1: barbican-api-keystone
    
    This change allows users to use ":" so that they can update the ini
    files with keeping it consistent with the default fules
    
    Depends-on: https://review.opendev.org/813614
    Change-Id: I528fdfe72f4c094695128186d43675480c163c8a

--- lib/puppet/type/designate_api_paste_ini.rb
+++ lib/puppet/type/designate_api_paste_ini.rb
@@ -45,6 +45,11 @@ Puppet::Type.newtype(:designate_api_paste_ini) do
     defaultto('<SERVICE DEFAULT>')
   end
 
+  newparam(:key_val_separator) do
+    desc 'The separator string to use between each setting name and value.'
+    defaultto('=')
+  end
+
   autorequire(:anchor) do
     ['designate::install::end']
   end
