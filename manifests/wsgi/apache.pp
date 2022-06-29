8f6bc169bbb75cfd33ca7627d2502336c0604dfa
Takashi Kajinami <tkajinam@redhat.com>
2022-06-30 08:41:14 +0900

    Expose headers option of apache::vhost
    The headers option in apache::vhost is required in some case, for
    example when adding the X-XSS-Protection header. This change allows
    customizing the option for the api vhost.
    
    This change also adds support for request_headers so that both request
    headers and response headers can customized.
    
    Change-Id: Iaa9627fe8ff3439a9662704c2008ff0e93717ae3

--- manifests/wsgi/apache.pp
+++ manifests/wsgi/apache.pp
@@ -85,6 +85,14 @@
 #   { python-path => '/my/python/virtualenv' }
 #   Defaults to {}
 #
+# [*headers*]
+#   (Optional) Headers for the vhost.
+#   Defaults to undef
+#
+# [*request_headers*]
+#   (Optional) Modifies collected request headers in various ways.
+#   Defaults to undef
+#
 # [*vhost_custom_fragment*]
 #   (optional) Passes a string of custom configuration
 #   directives to be placed at the end of the vhost configuration.
@@ -116,6 +124,8 @@ class designate::wsgi::apache (
   $access_log_format           = false,
   $error_log_file              = undef,
   $custom_wsgi_process_options = {},
+  $headers                     = undef,
+  $request_headers             = undef,
   $vhost_custom_fragment       = undef,
 ) {
 
@@ -149,6 +159,8 @@ class designate::wsgi::apache (
     wsgi_script_dir             => $::designate::params::designate_wsgi_script_path,
     wsgi_script_file            => 'app',
     wsgi_script_source          => $::designate::params::designate_wsgi_script_source,
+    headers                     => $headers,
+    request_headers             => $request_headers,
     access_log_file             => $access_log_file,
     access_log_format           => $access_log_format,
     error_log_file              => $error_log_file,
