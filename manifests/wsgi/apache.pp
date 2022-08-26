59a16624b79d19bd294de88a5573f121e2775c96
Takashi Kajinami <tkajinam@redhat.com>
2022-08-26 14:50:34 +0900

    Add Apache WSGI logging parameters for pipe/syslog
    Add parameters for advanced logging configurations in Apache to
    support piped logging and support for syslog (via mod_syslog
    available in Apache >= 2.5.0)
    
    Co-Authored-By: Andy Botting <andy@andybotting.com>
    Change-Id: Idaddbca1acb1daafbf5381fc2e19f7db4748bbea

--- manifests/wsgi/apache.pp
+++ manifests/wsgi/apache.pp
@@ -69,12 +69,29 @@
 #   (Optional) The log file name for the virtualhost.
 #   Defaults to undef
 #
+# [*access_log_pipe*]
+#   (Optional) Specifies a pipe where Apache sends access logs for
+#   the virtualhost.
+#   Defaults to undef
+#
+# [*access_log_syslog*]
+#   (Optional) Sends the virtualhost access log messages to syslog.
+#   Defaults to undef
+#
 # [*access_log_format*]
 #   (Optional) The log format for the virtualhost.
 #   Defaults to undef
 #
 # [*error_log_file*]
 #   (Optional) The error log file name for the virtualhost.
+#   Defaults to undef
+#
+# [*error_log_pipe*]
+#   (Optional) Specifies a pipe where Apache sends error logs for the virtualhost.
+#   Defaults to undef
+#
+# [*error_log_syslog*]
+#   (Optional) Sends the virtualhost error log messages to syslog.
 #   Defaults to undef
 #
 # [*custom_wsgi_process_options*]
@@ -121,8 +138,12 @@ class designate::wsgi::apache (
   $threads                     = 1,
   $priority                    = 10,
   $access_log_file             = undef,
+  $access_log_pipe             = undef,
+  $access_log_syslog           = undef,
   $access_log_format           = undef,
   $error_log_file              = undef,
+  $error_log_pipe              = undef,
+  $error_log_syslog            = undef,
   $custom_wsgi_process_options = {},
   $headers                     = undef,
   $request_headers             = undef,
@@ -161,9 +182,13 @@ class designate::wsgi::apache (
     wsgi_script_source          => $::designate::params::designate_wsgi_script_source,
     headers                     => $headers,
     request_headers             => $request_headers,
+    custom_wsgi_process_options => $custom_wsgi_process_options,
     access_log_file             => $access_log_file,
+    access_log_pipe             => $access_log_pipe,
+    access_log_syslog           => $access_log_syslog,
     access_log_format           => $access_log_format,
     error_log_file              => $error_log_file,
-    custom_wsgi_process_options => $custom_wsgi_process_options,
+    error_log_pipe              => $error_log_pipe,
+    error_log_syslog            => $error_log_syslog,
   }
 }
--- spec/classes/designate_wsgi_apache_spec.rb
+++ spec/classes/designate_wsgi_apache_spec.rb
@@ -20,11 +20,16 @@ describe 'designate::wsgi::apache' do
         :wsgi_script_dir             => platform_params[:wsgi_script_path],
         :wsgi_script_file            => 'app',
         :wsgi_script_source          => platform_params[:wsgi_script_source],
-        :access_log_file             => nil,
-        :access_log_format           => nil,
-        :custom_wsgi_process_options => {},
         :headers                     => nil,
         :request_headers             => nil,
+        :custom_wsgi_process_options => {},
+        :access_log_file             => nil,
+        :access_log_pipe             => nil,
+        :access_log_syslog           => nil,
+        :access_log_format           => nil,
+        :error_log_file              => nil,
+        :error_log_pipe              => nil,
+        :error_log_syslog            => nil,
       )}
     end
 
@@ -37,9 +42,6 @@ describe 'designate::wsgi::apache' do
           :ssl                         => true,
           :wsgi_process_display_name   => 'designate',
           :workers                     => 37,
-          :access_log_file             => '/var/log/httpd/access_log',
-          :access_log_format           => 'some format',
-          :error_log_file              => '/var/log/httpd/error_log',
           :custom_wsgi_process_options => {
             'python_path' => '/my/python/path',
           },
@@ -71,9 +73,70 @@ describe 'designate::wsgi::apache' do
         :custom_wsgi_process_options => {
           'python_path' => '/my/python/path',
         },
-        :access_log_file             => '/var/log/httpd/access_log',
-        :access_log_format           => 'some format',
-        :error_log_file              => '/var/log/httpd/error_log'
+      )}
+    end
+
+    context 'with custom access logging' do
+      let :params do
+        {
+          :access_log_format => 'foo',
+          :access_log_syslog => 'syslog:local0',
+          :error_log_syslog  => 'syslog:local1',
+        }
+      end
+
+      it { should contain_openstacklib__wsgi__apache('designate_wsgi').with(
+        :access_log_format => params[:access_log_format],
+        :access_log_syslog => params[:access_log_syslog],
+        :error_log_syslog  => params[:error_log_syslog],
+      )}
+    end
+
+    context 'with access_log_file' do
+      let :params do
+        {
+          :access_log_file => '/path/to/file',
+        }
+      end
+
+      it { should contain_openstacklib__wsgi__apache('designate_wsgi').with(
+        :access_log_file => params[:access_log_file],
+      )}
+    end
+
+    context 'with access_log_pipe' do
+      let :params do
+        {
+          :access_log_pipe => 'pipe',
+        }
+      end
+
+      it { should contain_openstacklib__wsgi__apache('designate_wsgi').with(
+        :access_log_pipe => params[:access_log_pipe],
+      )}
+    end
+
+    context 'with error_log_file' do
+      let :params do
+        {
+          :error_log_file => '/path/to/file',
+        }
+      end
+
+      it { should contain_openstacklib__wsgi__apache('designate_wsgi').with(
+        :error_log_file => params[:error_log_file],
+      )}
+    end
+
+    context 'with error_log_pipe' do
+      let :params do
+        {
+          :error_log_pipe => 'pipe',
+        }
+      end
+
+      it { should contain_openstacklib__wsgi__apache('designate_wsgi').with(
+        :error_log_pipe => params[:error_log_pipe],
       )}
     end
   end
