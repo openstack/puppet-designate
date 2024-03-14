1d4c78a694de8167cb729c9d87901d4bffdbea27
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-03-14 09:21:26 +0900

    Expose rabbit_transient_quorum_queue
    Depends-on: https://review.opendev.org/911021
    Change-Id: Idb975162d128112aaae1359b5d514cba59d180bf

--- manifests/init.pp
+++ manifests/init.pp
@@ -72,6 +72,10 @@
 #   (Optional) Use quorum queues in RabbitMQ.
 #   Defaults to $facts['os_service_default']
 #
+# [*rabbit_transient_quorum_queue*]
+#   (Optional) Use quorum queues for transients queues in RabbitMQ.
+#   Defaults to $facts['os_service_default']
+#
 # [*rabbit_quorum_delivery_limit*]
 #   (Optional) Each time a message is rdelivered to a consumer, a counter is
 #   incremented. Once the redelivery count exceeds the delivery limit
@@ -151,6 +155,7 @@ class designate(
   $rabbit_ha_queues                = $facts['os_service_default'],
   $rabbit_heartbeat_in_pthread     = $facts['os_service_default'],
   $rabbit_quorum_queue             = $facts['os_service_default'],
+  $rabbit_transient_quorum_queue   = $facts['os_service_default'],
   $rabbit_quorum_delivery_limit    = $facts['os_service_default'],
   $rabbit_quorum_max_memory_length = $facts['os_service_default'],
   $rabbit_quorum_max_memory_bytes  = $facts['os_service_default'],
@@ -196,6 +201,7 @@ class designate(
     heartbeat_in_pthread            => $rabbit_heartbeat_in_pthread,
     amqp_durable_queues             => $amqp_durable_queues,
     rabbit_quorum_queue             => $rabbit_quorum_queue,
+    rabbit_transient_quorum_queue   => $rabbit_transient_quorum_queue,
     rabbit_quorum_delivery_limit    => $rabbit_quorum_delivery_limit,
     rabbit_quorum_max_memory_length => $rabbit_quorum_max_memory_length,
     rabbit_quorum_max_memory_bytes  => $rabbit_quorum_max_memory_bytes,
