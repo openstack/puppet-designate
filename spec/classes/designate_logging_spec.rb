413ac43c4f9cf6f639f8690dc32f91c307777c01
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-09-22 01:53:35 +0900

    logging: Deprecate watch_log_file
    This option was deprecated in oslo.log 6.0.0 [1]. Because the warning
    to catch usage of the parameter is implemented in the underlying
    oslo::log defined resource type, no additional warning message is
    implemented in this module.
    
    [1] https://review.opendev.org/914788
    
    Depends-on: https://review.opendev.org/930126
    Change-Id: I0cdc0f69d8730bef996ded667e9b8ea2ea5a5d95

--- spec/classes/designate_logging_spec.rb
+++ spec/classes/designate_logging_spec.rb
@@ -67,7 +67,6 @@ describe 'designate::logging' do
         :syslog_log_facility => '<SERVICE DEFAULT>',
         :log_dir             => '/var/log/designate',
         :log_file            => '<SERVICE DEFAULT>',
-        :watch_log_file      => '<SERVICE DEFAULT>',
         :debug               => '<SERVICE DEFAULT>',
       )
     end
